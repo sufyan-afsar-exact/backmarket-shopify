dc2427273a2d4353eb3bf10cad8514dee9d2361a
Sufyan afsar <saifi8998@gmail.com>
2025-01-18 04:27:39 +0500

    Function to check BackMarket API connection using product listing endpoint

--- src/config/config.ts
+++ src/config/config.ts
@@ -6,3 +6,7 @@ export const SHOPIFY_API_KEY = process.env.SHOPIFY_API_KEY || '';
 export const SHOPIFY_API_SECRET = process.env.SHOPIFY_API_SECRET || '';
 export const SHOPIFY_API_TOKEN = process.env.SHOPIFY_API_TOKEN || '';
 export const SHOPIFY_SHOP_NAME = process.env.SHOPIFY_SHOP_NAME || '';
+export const BACKMARKET_API_KEY = process.env.BACKMARKET_API_KEY || '';
+export const BACKMARKET_CREDS = Buffer.from(`${process.env.BACKMARKET_EMAIL}:${process.env.BACKMARKET_PASSWORD}`).toString('base64');
+
+
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,8 @@ import Shopify from 'shopify-api-node';
 import app from './app';
 
 import dotenv from 'dotenv';
-import { SHOPIFY_API_TOKEN, SHOPIFY_SHOP_NAME } from './config/config';
+import { BACKMARKET_CREDS, SHOPIFY_API_TOKEN, SHOPIFY_SHOP_NAME } from './config/config';
+import axios from 'axios';
 dotenv.config();
 const PORT = process.env.PORT
 
@@ -10,7 +11,7 @@ const shopify = new Shopify({
     shopName: SHOPIFY_SHOP_NAME,
     accessToken: SHOPIFY_API_TOKEN
   });
-  
+
   // Shopify connection check
 shopify.shop.get()
 .then(shop => {
@@ -20,6 +21,29 @@ shopify.shop.get()
   console.error('Failed to connect to Shopify:', err.message);
 });
 
+// Function to check BackMarket API connection using product listing endpoint
+const checkBackMarketAPI = async () => {
+  try {
+    const response = await axios.get('https://preprod.backmarket.fr/ws/listings', {
+      headers: {
+        'Authorization': `Basic ${BACKMARKET_CREDS}`,  // Use generated token
+        'Accept': 'application/json',
+        'Accept-Language': 'fr-fr'
+      }
+    });
+    console.log(`Connected to BackMarket API : ${response.status}`);
+    return response.data;
+
+  } catch (error) {
+    const typedError = error as Error;
+    console.error('Invalid BackMarket API Token:', typedError.message);
+    throw new Error('Invalid BackMarket API Token');
+  }
+};
+
+
+// Call BackMarket API check
+checkBackMarketAPI();
   
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
