c24792e94dcf59126e2052e8d3ed24eda9703dac
Sufyan afsar <saifi8998@gmail.com>
2025-01-18 14:34:29 +0500

    call shopify produc listing

---
+++ src/controllers/shopifyController.ts
@@ -0,0 +1,11 @@
+import { BackMarketService } from "../services/backmarketService";
+import { ResponseWrapper } from "../helpers/response_wrapper";
+import { Request, Response } from "express";
+
+export class shopifyController {
+  public static async showProductListing(req: Request, res: Response) {
+    const backMarketService: BackMarketService = new BackMarketService();
+    const response: ResponseWrapper = new ResponseWrapper(res);
+    return response.ok(await backMarketService.showProductListing());
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 import Shopify from 'shopify-api-node';
 import app from './app';
-
 import dotenv from 'dotenv';
-import { BACKMARKET_API_URL, BACKMARKET_CREDS, SHOPIFY_API_TOKEN, SHOPIFY_SHOP_NAME } from './config/config';
-import axios from 'axios';
+import { SHOPIFY_API_TOKEN, SHOPIFY_SHOP_NAME } from './config/config';
+
+import { checkBackMarketAPI } from './services/shopifyService';
 dotenv.config();
 const PORT = process.env.PORT
 
-const shopify = new Shopify({
+export const shopify = new Shopify({
     shopName: SHOPIFY_SHOP_NAME,
     accessToken: SHOPIFY_API_TOKEN
   });
@@ -20,29 +20,6 @@ shopify.shop.get()
 .catch(err => {
   console.error('Failed to connect to Shopify:', err.message);
 });
-
-// Function to check BackMarket API connection using product listing endpoint
-const checkBackMarketAPI = async () => {
-  try {
-    const response = await axios.get(`${BACKMARKET_API_URL}/listings`, {
-      headers: {
-        'Authorization': `Basic ${BACKMARKET_CREDS}`,  // Use generated token
-        'Accept': 'application/json',
-        'Accept-Language': 'fr-fr'
-      }
-    });
-    console.log(`Connected to BackMarket API : ${response.status}`);
-    console.log(response.data);
-    return response.data;
-
-  } catch (error) {
-    const typedError = error as Error;
-    console.error('Invalid BackMarket API Token:', typedError.message);
-    throw new Error('Invalid BackMarket API Token');
-  }
-};
-
-
 // Call BackMarket API check
 checkBackMarketAPI();
   
